5adc58bc9e493ccb9912b579007b194cbb0e3d82
Linus Bergman <linusbergman@pm.me>
2021-04-22 13:11:12 +0300

    Fix parse logic and add Excel output

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # music-log-cleanup
-Work in progress...
+Work in progress... Test file is hardcoded while I work out how to use readline/tab-completion for the input.
--- music-log-cleanup.py
+++ music-log-cleanup.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 # Utility to parse and clean up xlsx exported from PGPTSession.
 
-import pandas
+import pandas as pd
 from timecode import Timecode
+from pprint import pprint
 
-readfile = pandas.read_excel(
+readfile = pd.read_excel(
     './EARTHRISE WILD RECOVERY-PG3 MUSIC LIST TRACK ORDER.xlsx')
 
-df = pandas.DataFrame(readfile)
+df = pd.DataFrame(readfile)
+
+# Rename usable columns to something sane
 df.rename(columns={
     "Unnamed: 0": "TC IN",
     "Unnamed: 1": "TC OUT",
@@ -15,12 +18,35 @@ df.rename(columns={
 },
           inplace=True)
 
+# Create new dataframe with only usable columns
 df1 = df[['Track Name', 'TC IN', 'TC OUT']]
+
+# Drop empty rows and sort by TC
 df1.dropna(subset=["Track Name"], inplace=True)
 df1.sort_values(['TC IN'], inplace=True)
 df1.reset_index(drop=True, inplace=True)
-df1['TC DURATION'] = df1.apply(
+
+tracklist = df1.values.tolist()
+tracks = [tracklist[0]]
+
+# Clean up overlapping tracks
+for currtrack in tracklist:
+    prevtrack = tracks[-1]
+    # Only check against first 10 characters of filename
+    if prevtrack[0][:10] == currtrack[0][:10] and Timecode(
+            '25', prevtrack[2]) >= Timecode('25', currtrack[1]):
+        tracks.pop()
+        tracks.append([prevtrack[0], prevtrack[1], currtrack[2]])
+    else:
+        tracks.append(currtrack)
+
+df2 = pd.DataFrame(tracks, columns=list(["Track Name", "TC IN", "TC OUT"]))
+df2.sort_values(['TC IN'], inplace=True)
+
+# Calculate duration
+df2['TC DURATION'] = df1.apply(
     lambda row: Timecode('25', row['TC OUT']) - Timecode('25', row['TC IN']),
     axis=1)
 
-print(df1)
+df2.to_excel("output.xlsx", index=False)
+pprint(df2)
